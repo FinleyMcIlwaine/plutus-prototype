0ba237bf400449976d471d02dd1cfb67c99511a0
David Smith <shmish111+github@gmail.com>
2020-03-04 12:57:50 +0000

    PS-based completion provider

--- marlowe-playground-client/src/Simulation.purs
+++ marlowe-playground-client/src/Simulation.purs
@@ -575,6 +575,7 @@ simulationPaneOld state =
   marloweEditor =
     aceComponent (Editor.initEditor initialContents StaticData.marloweBufferLocalStorageKey editorPreferences)
       (Just Live)
+
   monacoEditor = monacoComponent
 
   editorPreferences = view _editorPreferences state
